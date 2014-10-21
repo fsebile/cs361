10de5e31ad0bcf454e3e4bf3d2082d12d03cce9c
F. Sebile <fatimasebiletezcan@gmail.com>
2014-10-21 16:37:40 +0300

    I add an example of file check in.

--- Tests/Unit_Test.py
+++ Tests/Unit_Test.py
@@ -1,6 +1,11 @@
 __author__ = 'FSebile'
 
 import unittest
+import Main.movieRecommendations as mR
 
-class MytestCase(unittest.TestCase);
-    def test_something(self);
+class MytestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(mR.sqrt(4),2)
+
+if __name__ == '__main__':
+    unittest.main()
